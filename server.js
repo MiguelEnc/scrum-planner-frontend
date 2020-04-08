d920729c38f727bc1d0882811f42c2f61c146374
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-04-07 21:48:37 -0400

    Connect mongo

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Connect DataBase
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
+// Define routes
+app.use("/api/users", require("./routes/api/users"));
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
