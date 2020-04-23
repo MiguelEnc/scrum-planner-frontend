df4cfe33a244e6b2d61ab430a2942a18d85994a9
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-04-22 22:04:35 -0400

    add validation messages on project

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -6,7 +6,7 @@ const { check, validationResult } = require("express-validator");
 
 /**
  * @route       GET api/projects
- * @description Return projects of reuest user
+ * @description Retrieves projects of the given user
  * @access      Private
  */
 router.get("/", auth, async (req, res) => {
@@ -32,45 +32,49 @@ router.get("/", auth, async (req, res) => {
  * @description Create or update project
  * @access      Private
  */
-router.post("/", [auth, [check("name").not().isEmpty()]], async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { name } = req.body;
-  const { id } = req.user;
-
-  try {
-    let project = await Project.findOne({ $and: [{ name }, { users: id }] });
-
-    if (project) {
-      return res
-        .status(400)
-        .send("User already belong to a project with same name");
+router.post(
+  "/",
+  [auth, [check("name", "Name is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    // Create project
-    project = new Project({
-      name,
-      users: [id],
-      admins: [id],
-    });
-    await project.save();
+    const { name } = req.body;
+    const { id } = req.user;
 
-    // Add project to user
-    let user = await User.findById({ id });
-    await User.findOneAndUpdate(
-      { _id: id },
-      { projects: [...user.projects, project.id] }
-    );
+    try {
+      let project = await Project.findOne({ $and: [{ name }, { users: id }] });
 
-    res.json(project);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server error.");
+      if (project) {
+        return res
+          .status(400)
+          .send("User already belong to a project with same name");
+      }
+
+      // Create project
+      project = new Project({
+        name,
+        users: [id],
+        admins: [id],
+      });
+      await project.save();
+
+      // Add project to user
+      let user = await User.findById({ id });
+      await User.findOneAndUpdate(
+        { _id: id },
+        { projects: [...user.projects, project.id] }
+      );
+
+      res.json(project);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error.");
+    }
   }
-});
+);
 
 /**
  * @route       DELETE api/projects
@@ -79,7 +83,7 @@ router.post("/", [auth, [check("name").not().isEmpty()]], async (req, res) => {
  */
 router.delete(
   "/",
-  [auth, [check("name").not().isEmpty()]],
+  [auth, [check("name", "Name is required").not().isEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -97,7 +101,7 @@ router.delete(
       let user = await User.findById({ id });
       let updatedProjects = user.projects.filter((p) => p !== project.id);
       await User.findOneAndUpdate({ _id: id }, { projects: [updatedProjects] });
-      res.json("Project deleted");
+      res.json({ msg: "Project deleted" });
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
