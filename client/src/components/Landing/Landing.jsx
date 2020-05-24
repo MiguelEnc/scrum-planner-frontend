b178e4a8be57420f3343955c83980cc960cadef4
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-05-24 00:23:29 -0400

    refactor Landing layout

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,34 +1,33 @@
-import React, { Fragment } from "react";
-import bgImage from "../../assets/landing.jpeg";
+import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Row, Col } from "antd";
-
-const bgStyle = {
-  backgroundImage: "url(" + bgImage + ")",
-};
 
 const Landing = () => {
   return (
-    <Fragment>
-      <div className="bg-image" style={bgStyle}></div>
-      <div className="bg-text">
-        <Row justify="center">
-          <h1>Scrum Helper</h1>
-        </Row>
-        <Row justify="center">
-          <p>Retrospectives and planning poker for agile teams</p>
-        </Row>
-        <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-          <Col xs={10} offset={2}>
-            <Button type="primary" block>
-              Log In
-            </Button>
-          </Col>
-          <Col xs={10}>
-            <Button block>Sign Up</Button>
-          </Col>
-        </Row>
-      </div>
-    </Fragment>
+    <div id="landing">
+      <Row justify="center" id="landing__vertical">
+        <Col id="landing__col">
+          <Row justify="center">
+            <h1 id="landing__col__title">Scrum Planner</h1>
+          </Row>
+          <Row justify="center">
+            <p>Retrospectives and planning poker</p>
+          </Row>
+          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+            <Col xs={10} offset={2}>
+              <Button type="primary" block>
+                <Link to="/login">Log In</Link>
+              </Button>
+            </Col>
+            <Col xs={10}>
+              <Button block>
+                <Link to="/register">Sign Up</Link>
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
