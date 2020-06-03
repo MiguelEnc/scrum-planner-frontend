afb6b1fb4bba153aeecd6139c96205bb99ef4c5a
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-06-02 23:38:40 -0400

    create Jumbotron component

---
+++ client/src/components/Layout/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div className="jumbotron">
+      <h1 className="display-4">Scrum Planner</h1>
+      <p className="lead">Retrospectives and tasks stimation</p>
+    </div>
+  );
+};
+
+export default Jumbotron;
