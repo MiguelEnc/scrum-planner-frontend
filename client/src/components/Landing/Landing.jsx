d587c4e13b2bb997fc37c420ecf775c455ddbeff
Miguel Encarnacion <miguelencarnacion7@gmail.com>
2020-06-02 23:41:34 -0400

    add login and jumbotron to Landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Row, Col } from "antd";
+import { Row, Col } from "antd";
+import Jumbotron from "../Layout/Jumbotron/Jumbotron";
+import LoginForm from "../auth/LoginForm/LoginForm";
+
+import bgImage from "../../assets/landing.jpg";
+const bgStyle = {
+  backgroundImage: "url(" + bgImage + ")",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  height: "100%",
+};
 
 const Landing = () => {
   return (
-    <div id="landing">
-      <Row justify="center" id="landing__vertical">
-        <Col id="landing__col">
-          <Row justify="center">
-            <h1 id="landing__col__title">Scrum Planner</h1>
-          </Row>
-          <Row justify="center">
-            <p>Retrospectives and planning poker</p>
-          </Row>
-          <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-            <Col xs={10} offset={2}>
-              <Button type="primary" block>
-                <Link to="/login">Log In</Link>
-              </Button>
-            </Col>
-            <Col xs={10}>
-              <Button block>
-                <Link to="/register">Sign Up</Link>
-              </Button>
-            </Col>
-          </Row>
+    <div id="bg" style={bgStyle}>
+      <Row id="landing__row">
+        <Col xs={24} lg={12} xxl={{ span: 10, offset: 2 }}>
+          <Jumbotron />
+        </Col>
+        <Col xs={24} lg={12}>
+          <LoginForm />
         </Col>
       </Row>
     </div>
--- client/src/components/auth/LoginForm/LoginForm.jsx
+++ client/src/components/auth/LoginForm/LoginForm.jsx
@@ -10,79 +10,77 @@ const LoginForm = () => {
   };
 
   return (
-    <Row justify="end" id="login__row">
-      <Col id="login__col">
-        <Form
-          name="normal_login"
-          className="login-form"
-          style={{ width: "270px" }}
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
+    <div id="login__col">
+      <Form
+        name="normal_login"
+        className="login-form"
+        style={{ width: "270px" }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Item
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please fill out your Email!",
+            },
+          ]}
         >
-          <Item
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: "Please fill out your Email!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="Email"
-            />
-          </Item>
-          <Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please fill out your Password!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Item>
-          <Item>
-            <Row>
-              <Col xs={12}>
-                <Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Remember me</Checkbox>
-                </Item>
-              </Col>
-              <Col xs={12} id="login__forgot-wrapper">
-                <a className="login-form-forgot" href="#">
-                  Forgot password
-                </a>
-              </Col>
-            </Row>
-          </Item>
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Email"
+          />
+        </Item>
+        <Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please fill out your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Item>
+        <Item>
+          <Row>
+            <Col xs={12}>
+              <Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Item>
+            </Col>
+            <Col xs={12} id="login__forgot-wrapper">
+              <a className="login-form-forgot" href="#">
+                Forgot password
+              </a>
+            </Col>
+          </Row>
+        </Item>
 
-          <Item>
-            <Button
-              block
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              <Link to="/dashboard">Log in</Link>
-            </Button>
-          </Item>
-          <Item noStyle>
-            <p style={{ marginBottom: 0 }}>
-              Or <Link to="/register">register now!</Link>
-            </p>
-          </Item>
-        </Form>
-      </Col>
-    </Row>
+        <Item>
+          <Button
+            block
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            <Link to="/dashboard">Log in</Link>
+          </Button>
+        </Item>
+        <Item noStyle>
+          <p style={{ marginBottom: 0 }}>
+            Or <Link to="/register">register now!</Link>
+          </p>
+        </Item>
+      </Form>
+    </div>
   );
 };
 
